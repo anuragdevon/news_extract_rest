8127ca08ef73e218c51e057f255902543623ac8c
anuragdevon <msm19b021@iiitdm.ac.in>
2023-03-28 15:25:15 +0530

    add flask scrapper

---
+++ scrape.py
@@ -0,0 +1,75 @@
+import csv
+from datetime import datetime
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import psycopg2
+from dotenv import load_dotenv
+from os import getenv
+
+from .utils import extract_data
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+CORS(app, origins=["*"])
+
+# PostgreSQL connection
+conn = psycopg2.connect(database=getenv("database"), user=getenv("user"), password=getenv("password"), host=getenv("host"), port=getenv("port"))
+cur = conn.cursor()
+
+#------------------------------------------------------APIS START--------------------------------------------------------#
+@app.route('/parse_url', methods=['POST'])
+def parse_url():
+    url = request.json.get('url', '')
+    if url:
+        raw_data, error = extract_data(url)
+        if error == "":
+            # Store data in CSV file
+            csv_filename = datetime.now().strftime("%d%m%Y") + "_verge.csv"
+            with open(csv_filename, mode='a', newline='') as csv_file:
+                writer = csv.writer(csv_file)
+                writer.writerow(['', raw_data['link'], raw_data['title'], raw_data['author'], raw_data['pub_date']])
+
+            # # Store data in PostgreSQL
+            cur.execute("INSERT INTO your_table_name (url, title, author, pub_date) VALUES (%s, %s, %s, %s)",
+                        (raw_data['link'], raw_data['title'], raw_data['author'], raw_data['pub_date']))
+            conn.commit()
+            result = {
+                "message": "Data parsed and stored successfully!",
+                "output": {
+                    "title": raw_data["title"],
+                    "url": raw_data["link"],
+                    "author": raw_data["author"],
+                    "published_date": raw_data["pub_date"]
+                }
+            }
+            return jsonify(result), 200
+        else:
+            return jsonify({"success": False, "error": error}), 400
+    else:
+        return jsonify({"success": False, "error": "URL not provided"}), 400
+
+
+
+@app.route('/get_data/<int:id>', methods=['GET'])
+def get_data(id):
+    cur.execute("SELECT * FROM your_table_name WHERE id = %s", (id,))
+    data = cur.fetchone()
+    if data:
+        result = {
+            "id": data[0],
+            "url": data[1],
+            "title": data[2],
+            "author": data[3],
+            "published_date": data[4].strftime("%Y-%m-%d %H:%M:%S")  # format date as string
+        }
+        return jsonify(result), 200
+    else:
+        return jsonify({"success": False, "error": "Data not found"}), 404
+
+#------------------------------------------------------APIS END--------------------------------------------------------#
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+def extract_data(url: str) -> dict:
+    results = {}
+    error = ""
+    try:
+        # Make a request
+        response = requests.get(url)
+
+        # Parse the HTML content
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        results["author"] = soup.find('meta', {'name': 'parsely-author'})['content']
+
+        results["title"] = soup.find('meta', {'name': 'parsely-title'})['content']
+
+        results["pub_date"] = soup.find('meta', {'name': 'parsely-pub-date'})['content']
+
+        results["link"] = soup.find('meta', {'name': 'parsely-link'})['content']
+
+    except Exception as e:
+        error = str(e)
+
+    return results, error
